50f28f3166913647b117d2f534199c905f069b39
git-muggle <107381351+git-muggle@users.noreply.github.com>
2022-08-28 20:48:21 +0800

    Add files via upload

---
+++ TcpServer/mythread.cpp
@@ -0,0 +1,20 @@
+#include "mythread.h"
+
+myThread::myThread(QTcpSocket *s)
+{
+    this->socket = s;
+}
+
+void myThread::run()
+{
+    connect(socket, &QTcpSocket::readyRead, this, &myThread::clientINfoSlot);
+}
+
+//不能在其他类里面操作界面
+void myThread::clientINfoSlot()
+{
+    //qDebug() << socket->readAll();
+    QByteArray ba = socket->readAll();
+    emit sendToWidget(ba);//发送信号
+
+}
